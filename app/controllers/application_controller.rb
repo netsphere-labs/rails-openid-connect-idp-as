6e3d3c6ca9a3521e6059aff0a86c2fc38f85d11b
5467ae7ee55e9a3933a3c89b0d4d446b1e03b8b1 <jim.baker@auth0.com>
2019-09-17 12:24:58 -0700

    Fixed some commented code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,14 @@ class ApplicationController < ActionController::Base
     render status: e.status, nothing: true
   end
 
+  # Commented out Facebook by JB for this fork:
   # rescue_from FacebookOAuth::Exception, Rack::OAuth2::Client::Error do |e|
   #   redirect_to root_url, flash: {error: e.message}
   # end
+
+  rescue_from Rack::OAuth2::Client::Error do |e|
+    redirect_to root_url, flash: {error: e.message}
+  end
 
   protect_from_forgery
 end
