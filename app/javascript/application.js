30788570f3e96660b2c3bb0124d6c111239346bf
Hisashi Horikawa <hisashi.horikawa@gmail.com>
2024-05-18 21:47:54 +0900

    wip.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,11 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
+import Rails from "@rails/ujs";
 // import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "./channels"
+import * as ActiveStorage from "@rails/activestorage";
+import "./channels";
 
-Rails.start()
+// All of bulma
+//import Bulma from '@vizuaalog/bulmajs';
+
+Rails.start();
 // Turbolinks.start()
-ActiveStorage.start()
+ActiveStorage.start();
+
